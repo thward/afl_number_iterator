6d484ee11e4150cb7e541817a79097af451d8dd6
thward <thomas.h.ward@gmail.com>
2018-02-12 18:25:23 +1100

    Added client side validation

--- src/main/java/numberiterator/NumberIterator.java
+++ src/main/java/numberiterator/NumberIterator.java
@@ -2,10 +2,12 @@ package numberiterator;
 
 import java.util.Scanner;
 
+import org.apache.commons.lang3.StringUtils;
+
 import numberiterator.controller.DividesByController;
 
 /**
- * Calls iterator controller to determine if dividend can be divided by supplied divisors  
+ * Command line client to determine if dividend can be divided by supplied divisors  
  * 
  * @author Tom Ward
  * @version 1.0
@@ -18,9 +20,21 @@ public class NumberIterator {
 		Scanner scanner = new Scanner(System.in);
 		
 		while (carryOn){
-						
-			System.out.print("Enter sequence size: ");
-			Integer sequenceSize = Integer.parseInt(scanner.nextLine());
+			
+			String sequenceSizeChar = null;
+			Integer sequenceSize = 0;
+			
+			boolean sequenceSizeIsValid = false;
+			while (!sequenceSizeIsValid){
+				System.out.print("Enter sequence size: ");
+				sequenceSizeChar = scanner.nextLine();
+				if (!StringUtils.isNumeric(sequenceSizeChar))
+					System.out.println("Sequence size must be numeric, and greater than or equal to zero");
+				else
+					sequenceSizeIsValid = true;
+			}
+			
+			sequenceSize = Integer.parseInt(sequenceSizeChar);
 			
 			DividesByController.iterateSequence(sequenceSize, 3, 5);
 		
